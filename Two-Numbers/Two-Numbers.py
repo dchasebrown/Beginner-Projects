7c024abed3a4a82f2d6d19d8017e3aa1592560af
Dakota Brown <brown.dakotachase@gmail.com>
2019-02-13 14:51:01 -0500

    Two-Numbers Version 1

---
+++ Two-Numbers/Two-Numbers.py
@@ -0,0 +1,13 @@
+def sum_for_target(numbers, target):
+    num_history = set()
+    for num in numbers:
+        compliment = target - num
+        if compliment in num_history:
+            index1 = numbers.index(compliment)
+            index2 = numbers.index(num)
+            return([index1, index2])
+        else:
+            num_history.add(num)  
+
+index = sum_for_target([2,11,15,10,7,3], 9)
+print(index)
